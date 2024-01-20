9efe7e9519685f6fbceb9d4d8e17f791bcb363a3
johansitanggang <johanstg45@gmail.com>
2024-01-20 22:22:23 +0700

    edit part5.5

--- part5.5/soal1-4.js
+++ part5.5/soal1-4.js
@@ -7,18 +7,18 @@
 // //  untuk membentuk sebuah kalimat. Jangan lupa menambahkan spasi di setiap kata, dan
 // //   tampilkan di console hasil penggabungannya! Kamu tidak perlu membuat variable baru!
 
-// let word = "JavaScript";
-// let second = "is";
-// let third = "awesome";
-// let fourth = "and";
-// let fifth = "I";
-// let sixth = "love";
-// let seventh = "it!";
+let word = "JavaScript";
+let second = "is";
+let third = "awesome";
+let fourth = "and";
+let fifth = "I";
+let sixth = "love";
+let seventh = "it!";
 
 // //code here
 
 // // ---------------------------------------
-// console.log(word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh);
+console.log(word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh);
 // // ---------------------------------------
 
 // ========== SOAL 1 END ==========
@@ -34,21 +34,21 @@
 // // Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
 // //  tersebut!
 
-// let word = "wow JavaScript is so cool";
-// let exampleFirstWord = word[0] + word[1] + word[2];
+let word2 = "wow JavaScript is so cool";
+let exampleFirstWord = word2[0] + word2[1] + word2[2];
 
-// console.log("First Word: " + exampleFirstWord);
+console.log("First Word: " + exampleFirstWord);
 
 // // ---------------------------------------
-// let secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13];
-// let thirdWord = word[15] + word[16];
-// let fourthWord = word[18] + word[19];
-// let fifthWord = word[21] + word[22] + word[23] + word[24];
+let secondWord = word2[4] + word2[5] + word2[6] + word2[7] + word2[8] + word2[9] + word2[10] + word2[11] + word2[12] + word2[13];
+let thirdWord = word2[15] + word2[16];
+let fourthWord = word2[18] + word2[19];
+let fifthWord = word2[21] + word2[22] + word2[23] + word2[24];
 
-// console.log("Second Word: " + secondWord);
-// console.log("Third Word: " + thirdWord);
-// console.log("Fourth Word: " + fourthWord);
-// console.log("Fifth Word: " + fifthWord);
+console.log("Second Word: " + secondWord);
+console.log("Third Word: " + thirdWord);
+console.log("Fourth Word: " + fourthWord);
+console.log("Fifth Word: " + fifthWord);
 // // ---------------------------------------
 // ========== SOAL 2 END ===========
 
@@ -57,20 +57,20 @@
 // // Problem
 // // Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
 
-// let word3 = "wow JavaScript is so cool";
-// let exampleFirstWord3 = word3.substring(0, 3);
+let word3 = "wow JavaScript is so cool";
+let exampleFirstWord3 = word3.substring(0, 3);
 
-// console.log("First Word: " + exampleFirstWord3);
+console.log("First Word: " + exampleFirstWord3);
 
 // // ---------------------------------------
-// let secondWord3 = word3.substring(4, 14);
-// let thirdWord3 = word3.substring(15, 17);
-// let fourthWord3 = word3.substring(18, 20);
-// let fifthWord3 = word3.substring(21, 25);
-// console.log("Second Word: " + secondWord3);
-// console.log("Third Word: " + thirdWord3);
-// console.log("Fourth Word: " + fourthWord3);
-// console.log("Fifth Word: " + fifthWord3);
+let secondWord3 = word3.substring(4, 14);
+let thirdWord3 = word3.substring(15, 17);
+let fourthWord3 = word3.substring(18, 20);
+let fifthWord3 = word3.substring(21, 25);
+console.log("Second Word: " + secondWord3);
+console.log("Third Word: " + thirdWord3);
+console.log("Fourth Word: " + fourthWord3);
+console.log("Fifth Word: " + fifthWord3);
 // // ---------------------------------------
 // ========== SOAL 3 END ==========
 
